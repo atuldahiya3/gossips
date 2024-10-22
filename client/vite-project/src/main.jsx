89776b8f7b1c9fc522d90b3e8ae1fe3e1c3d6b35
atulThynk <87331579+atuldahiya3@users.noreply.github.com>
2024-10-22 21:12:12 +0530

    redux setup

--- client/vite-project/src/main.jsx
+++ client/vite-project/src/main.jsx
@@ -1,17 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import {CssBaseline} from '@mui/material'
-import {HelmetProvider} from 'react-helmet-async'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { CssBaseline } from "@mui/material";
+import { HelmetProvider } from "react-helmet-async";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HelmetProvider>
-      <CssBaseline/>
-      <div onClick={(e)=>e.preventDefault()}>
-        <App />
-      </div>
-    </HelmetProvider>
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <HelmetProvider>
+        <CssBaseline />
+        <div onClick={(e) => e.preventDefault()}>
+          <App />
+        </div>
+      </HelmetProvider>
+    </Provider>
+  </StrictMode>
+);
