5da04f636e7e7b1cc6d55a86951e29ba3a9a122f
atulThynk <87331579+atuldahiya3@users.noreply.github.com>
2024-12-28 14:37:29 +0530

    socket.io backend setup

--- server/lib/helper.js
+++ server/lib/helper.js
@@ -4,8 +4,8 @@ export const getOtherMembers=(members,user_id)=>{
     return members.find((member)=>member._id.toString() !==user_id.toString())
 }
 
-// export const getSockets=(users)=>{
-//     return users.map((user)=> userSocketIds.get(user._id.toString()))
-// }
+export const getSockets=(users)=>{
+    return users.map((user)=> userSocketIds.get(user._id.toString()))
+}
 
-export const getBase64=(file)=> `data:${file.mimetype};base64,${file.buffer.toString("base64")}`
+export const getBase64=(file)=> `data:${file.mimetype};base64,${file.buffer.toString("base64")}`  
