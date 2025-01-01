01453dcb5d7ae7b7395a021fb23327841c51e8ab
atulThynk <87331579+atuldahiya3@users.noreply.github.com>
2025-01-01 23:37:40 +0530

    socket frotnend setup

---
+++ client/vite-project/src/socket.jsx
@@ -0,0 +1,22 @@
+import { createContext, useContext } from "react";
+import { io } from "socket.io-client";
+
+const SocketContext = createContext();
+
+const getSocket=()=>useContext(SocketContext)
+
+const socketProvier = ({ children }) => {
+  const socket = useMemo(
+    () =>
+      io("https://localhost:3000", {
+        withCredentials: true,
+        auth: { token: localStorage.getItem("token") },
+      }),
+    []
+  );
+  return (
+    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
+  );
+};
+
+export {getSocket,socketProvier}
