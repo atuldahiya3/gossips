02d32a2e5262bc407d426d769f867f20d50fdae0
atulThynk <87331579+atuldahiya3@users.noreply.github.com>
2025-01-06 22:28:36 +0530

    middleware update

--- server/App.js
+++ server/App.js
@@ -15,94 +15,95 @@ import cors from "cors";
 import { v2 as cloudinary } from "cloudinary";
 import { Message } from "./models/message.js";
 import { corsOption } from "./constants/config.js";
+import { socketAuthenticator } from "./middlewares/auth.js";
 
 dotenv.config({
-    path:'./.env'
-})
+  path: "./.env",
+});
 
-const mongoURI=process.env.MONGO_URI
+const mongoURI = process.env.MONGO_URI;
 
-const userSocketIds=new Map()
+const userSocketIds = new Map();
 
 connectDB(mongoURI);
 cloudinary.config({
-    cloud_name:process.env.CLOUDINARY_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_API_SECRET
-})
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 // createUser(10);
-const app=express();
-const server=createServer(app)
-const io=new Server(server,{
-    cors:corsOption
-})
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: corsOption,
+});
 //using middleware to access values
-app.use(express.json());  // to access json data  
-app.use(express.urlencoded());  // to access form data
+app.use(express.json()); // to access json data
+app.use(express.urlencoded()); // to access form data
 
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use(cors(corsOption))
+app.use(cors(corsOption));
 
-app.use("/api/v1/user",userRoute)
-app.use("/api/v1/chat",chatRoute)
-app.use("/api/v1/admin",adminRoute)
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/chat", chatRoute);
+app.use("/api/v1/admin", adminRoute);
 
-app.get("/",(req,res)=>{
-    res.send(" i Am HOmE")
-})
+app.get("/", (req, res) => {
+  res.send(" i Am HOmE");
+});
 
-io.use((socket,next)=>{
-    cookieParser()(socket.request,socket.request.res,()=>{
-        next();
-    })
-})
+io.use((socket, next) => {
+  cookieParser()(socket.request, socket.request.res, async(err) => {
+    socketAuthenticator(err,socket,next);
+  });
+});
 
-io.on("connection",(socket)=>{
-    socket.handshake.query.auth
-    const user={
-        _id:"abab",
-        name:"atul"
-    }
-    userSocketIds.set(user._id.toString(),socket.id)
-    console.log("A user connected",socket.id);
+io.on("connection", (socket) => {
+  socket.handshake.query.auth;
+  const user = {
+    _id: "abab",
+    name: "atul",
+  };
+  userSocketIds.set(user._id.toString(), socket.id);
+  console.log("A user connected", socket.id);
 
-    socket.on(NEW_MESSAGE,async({chatId, members, messages})=>{
-        const messageForRealTime={
-            content:messages,
-            _id:v4(),
-            sender:{
-                _id:user._id,
-                name:user.name
-            },
-            chat:chatId,
-            cretedAt:new Date().toISOString()
-        }
+  socket.on(NEW_MESSAGE, async ({ chatId, members, messages }) => {
+    const messageForRealTime = {
+      content: messages,
+      _id: v4(),
+      sender: {
+        _id: user._id,
+        name: user.name,
+      },
+      chat: chatId,
+      cretedAt: new Date().toISOString(),
+    };
 
-        const messageForDb={
-            content:messages,
-            sender:user._id,
-            chat:chatId,
-        }
+    const messageForDb = {
+      content: messages,
+      sender: user._id,
+      chat: chatId,
+    };
 
-        const membersSocket=getSockets(members)
-        io.to(membersSocket).emit(NEW_MESSAGE,{
-            chatId,
-            message:messageForRealTime
-        })
-        io.to(membersSocket).emit(NEW_MESSAGE_ALERT,{chatId})
-        await Message.create(messageForDb)
-        console.log("new message",messageForRealTime);
-    })
+    const membersSocket = getSockets(members);
+    io.to(membersSocket).emit(NEW_MESSAGE, {
+      chatId,
+      message: messageForRealTime,
+    });
+    io.to(membersSocket).emit(NEW_MESSAGE_ALERT, { chatId });
+    await Message.create(messageForDb);
+    console.log("new message", messageForRealTime);
+  });
 
-    socket.on("disconnect",()=>{
-        console.log("disconnected");
-        userSocketIds.delete(user._id.toString())
-    })
-})
+  socket.on("disconnect", () => {
+    console.log("disconnected");
+    userSocketIds.delete(user._id.toString());
+  });
+});
 
-server.listen((3000),()=>{
-    console.log("Server is running on port 3000");
-})
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
 
 // export { userSocketIds };
